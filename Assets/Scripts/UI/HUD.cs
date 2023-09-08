dea80bd21c7c6bb13bc0071697bed2980dbf81de
kim ju hyung <kim52116@naver.com>
2023-09-08 15:19:00 +0900

    playing UI
    기본 배치

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+
+    TextMeshProUGUI myText;
+    Slider myslider;
+
+    private void Awake()
+    {
+        myText = GetComponent<TextMeshProUGUI>();
+        myslider = GetComponent<Slider>();
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+                break;
+            case InfoType.Level:
+                break;
+            case InfoType.Kill:
+                break;
+            case InfoType.Time:
+                break;
+            case InfoType.Health:
+                break;
+        }
+    }
+}
