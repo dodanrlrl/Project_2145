100c540d829253cc9ed0eae64252cd4d90bff97f
noyyo <dbswltjr7@gmail.com>
2023-09-08 16:41:49 +0900

    캐릭터 기본

---
+++ Assets/Scripts/Entities/TopDownCharacter.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacter : MonoBehaviour
+{
+    public string Name { get; private set; }
+    public int CurrentHP { get; private set; }
+    public int MaxHP { get; private set; }
+    public float AttackDelay { get; private set; }
+    public float Speed { get; private set; }
+    public List<GameObject> Projectiles = new List<GameObject>();
+    public GameObject CurrentProjectile { get; private set; }
+
+    public void SetCharacterInfo(string name, int maxHp, float attackDelay)
+    {
+        Name = name;
+        MaxHP = maxHp;
+        AttackDelay = attackDelay;
+    }
+    public void ChangeCurrentProjectile()
+    {
+        if (Projectiles == null || Projectiles.Count == 0)
+            return;
+
+        int nextIndex = Projectiles.IndexOf(CurrentProjectile) + 1;
+        if (nextIndex >= Projectiles.Count)
+        {
+            nextIndex = 0;
+        }
+        CurrentProjectile = Projectiles[nextIndex];
+    }
+}
---
+++ Assets/Scripts/Entities/TopDownShooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownShooting : MonoBehaviour
+{
+    private TopDownCharacterController _controller;
+    private TopDownCharacter _character;
+    public GameObject Spawner;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacterController>();
+        _character = GetComponent<TopDownCharacter>();
+    }
+    private void Start()
+    {
+        _controller.OnShootEvent += Shoot;
+    }
+    public void Shoot()
+    {
+        GameObject projectile = Instantiate(_character.CurrentProjectile);
+        projectile.transform.position = Spawner.transform.position;
+        projectile.transform.rotation = transform.rotation;
+    }
+}
