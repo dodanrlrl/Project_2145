7391f6f51f7f6a03e57afbb5bc559583e3e05aa8
noyyo <dbswltjr7@gmail.com>
2023-09-13 19:40:22 +0900

    Squashed commit of the following:
    commit d383da45afdb56f02af26747a108bf1842f1bde0
    Author: noyyo <dbswltjr7@gmail.com>
    Date:   Wed Sep 13 19:35:27 2023 +0900
    
        움직임 패턴구현 및 총알 투사체 버그픽스

--- Assets/Scripts/Controller/TopDownEnemyController.cs
+++ Assets/Scripts/Controller/TopDownEnemyController.cs
@@ -3,25 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class TopDownEnemyController : TopDownCharacterController
 {
-    private List<Func<IEnumerator>> _movePatterns = new List<Func<IEnumerator>>();
-    private Vector2 moveDirection = Vector2.zero;
+    private List<IEnumerator> _movePatterns = new List<IEnumerator>();
+    protected const string PLAYER_UI_BAR = "PlayerUIBar";
 
     // 현재 테스트용
-    private new void Start()
+    protected override void Start()
     {
         base.Start();
-    }
-    private void OnEnable()
-    {
-        AddMovePattern(TestMovePattern);
-        OnMove();
+
+        StartCoroutine(OnMove());
         StartCoroutine(OnLook());
         StartCoroutine(OnShoot());
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    protected virtual void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Boundary")
         {
@@ -30,47 +28,63 @@ public class TopDownEnemyController : TopDownCharacterController
         }
         // 임시로 파괴하게 만들었고 차후에 오브젝트 풀에 반납해야함. 
     }
-    protected virtual void OnMove()
+    protected virtual IEnumerator OnMove()
     {
-        //// 객체별로 패턴 짜주기
-        //if (_movePatterns.Count == 0)
-        //{
-        //    while (true)
-        //    {
-        //        moveDirection = new Vector2(0, -1);
-        //        CallMoveEvent(moveDirection);
-        //        yield return null;
-        //    }
-        //}
+        //패턴이 없으면 기본 패턴 추가
+        if (_movePatterns.Count == 0)
+        {
+            AddMovePattern(DefaultMovePattern());
+        }
 
-        StartCoroutine(_movePatterns[0]());
+        foreach (IEnumerator pattern in _movePatterns)
+        {
+            yield return StartCoroutine(pattern);
+        }
+        // 패턴을 다 끝마쳤으면 기본 패턴으로 움직여서 멈추는 것을 방지.
+        StartCoroutine(DefaultMovePattern());
+
     }
     protected virtual IEnumerator OnLook()
     {
         // 객체별로 패턴 짜주기
+        Vector2 playerPosition = Vector2.zero;
+        Vector2 direction = Vector2.zero;
         while (true)
         {
             // 게임 매니저에게서 플레이어 받아서 해당위치 바라봄 
-            CallLookEvent(GameManager.Instance.player.transform.position);
+            playerPosition = GameManager.Instance.player.transform.position;
+            direction = playerPosition - (Vector2)gameObject.transform.position;
+            CallLookEvent(direction);
             yield return null;
         }
     }
     protected virtual IEnumerator OnShoot()
     {
         // 차후에 혹시 샷을 멈춰야 하는 기획이 생기면 작성
+        Camera _camera = Camera.main;
+        float camHeightSize = _camera.orthographicSize * 2;
+        float meterPerPixel = camHeightSize / Screen.height;
+        float halfWidthSize = meterPerPixel * Screen.width / 2;
+        float halfHeightSize = _camera.orthographicSize;
+        Vector2 sizeDelta = GameManager.Instance.PlayerUI.transform.Find(PLAYER_UI_BAR).GetComponent<RectTransform>().sizeDelta;
+        float playerUIHeight = sizeDelta.y * meterPerPixel;
+
         while (true)
         {
-            IsShooting = true;
+            if (transform.position.x < -halfWidthSize || transform.position.x > halfWidthSize)
+                IsShooting = false;
+            else if (transform.position.y < -halfHeightSize + playerUIHeight || transform.position.y > halfHeightSize)
+                IsShooting = false;
+            else
+                IsShooting = true;
             yield return null;
         }
     }
-    public void AddMovePattern(Func<IEnumerator> movePattern)
+    public void AddMovePattern(IEnumerator movePattern)
     {
         _movePatterns.Add(movePattern);
     }
-
-    // 현재 테스트용
-    public IEnumerator TestMovePattern()
+    protected IEnumerator DefaultMovePattern()
     {
         while(true)
         {
@@ -78,4 +92,5 @@ public class TopDownEnemyController : TopDownCharacterController
             yield return null;
         }
     }
+
 }
--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -8,3 +8,20 @@ public enum ProjectileType
     CannonBullet,
     PowerUpCannonBullet,
 }
+
+public enum MovePatternStartPosition
+{
+    UpperLeft,
+    UpperRight,
+    LowerLeft,
+    LowerRight,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+public enum MovePatternRotation
+{
+    Clockwise,
+    CounterClockwise,
+}
